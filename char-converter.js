0c7918422150a0421c0e8a505e8662674cc5ac14
justype <3424005241@qq.com>
2021-03-03 19:54:49 +0800

    增加了更多的选项

--- char-converter.js
+++ char-converter.js
@@ -17,12 +17,19 @@ Superscript["+"] = "⁺";
 Superscript["-"] = "⁻";
 
 
-function normal_to_chemical(text) {
-    return text.replace(/[\w\d][\+\-]/g, c => c[0] + Superscript[c[1]])
+function normalToChemical(text) {
+    return text.replace(/[\w\d][\+\-]/g, c => c[0] + Superscript[c[1]]) // 识别 数字、字母后面的
         .replace(/\w\d+/g, c => {
             r = c[0];
             for (let i = 1; i < c.length; i++)
                 r += Subscript[c[i]];
             return r;
         });
+}
+
+function fixAndToChemical(text) {
+    let fixText = text.replace(/c[1l]/ig, "Cl") // 目前只发现 Cl 可能拼错
+                    //   .replace(/(?<=[\u4e00-\u9fa5，（《])[\n ]+(?=[\u4e00-\u9fa5，、）》])/g, ""); // 去除中文间 多余的换行和空格
+                      .replace(/(?<![\.。;；:：])[\n]+/g, ""); // 去除多余的换行
+    return normalToChemical(fixText);
 }
